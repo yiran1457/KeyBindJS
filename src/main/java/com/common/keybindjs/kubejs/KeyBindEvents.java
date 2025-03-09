f0d86c687e17970f859d8d56466e5778ceb41081
yiran1457 <3270897489@qq.com>
2025-03-09 21:11:08 +0800

    1.9更新

--- src/main/java/com/common/keybindjs/kubejs/KeyBindEvents.java
+++ src/main/java/com/common/keybindjs/kubejs/KeyBindEvents.java
@@ -9,5 +9,9 @@ public interface KeyBindEvents {
     EventHandler KEY_BINDING = GROUP.startup("register",()-> KeyBindEvent.class);
     EventHandler KEY_MODIFY = GROUP.startup("modify",()-> KeyBindModifyEvent.class);
     EventHandler KEY_PRESS = GROUP.client("keyPress",()->KeyPressedEvent.class).extra(Extra.STRING);
-    EventHandler KEY_PRESS_GUI = GROUP.client("keyPressInGui",()->KeyPressInGuiEvent.class).extra(Extra.STRING);
+    EventHandler KEY_RELEASE = GROUP.client("keyRelease",()->KeyPressedEvent.class).extra(Extra.STRING);
+    EventHandler FIRST_KEY_PRESS = GROUP.client("firstKeyPress",()->KeyPressedEvent.class).extra(Extra.STRING);
+    EventHandler KEY_PRESS_GUI = GROUP.client("keyPressInGui",()->KeyPressedEvent.class).extra(Extra.STRING);
+    EventHandler KEY_RELEASE_GUI = GROUP.client("keyReleaseInGui",()->KeyPressedEvent.class).extra(Extra.STRING);
+    EventHandler FIRST_KEY_PRESS_GUI = GROUP.client("firstKeyPressInGui",()->KeyPressedEvent.class).extra(Extra.STRING);
 }
--- src/main/java/com/common/keybindjs/kubejs/KeyPressedEvent.java
+++ src/main/java/com/common/keybindjs/kubejs/KeyPressedEvent.java
@@ -3,15 +3,22 @@ package com.common.keybindjs.kubejs;
 import com.common.keybindjs.KeyBindJS;
 import dev.latvian.mods.kubejs.client.ClientEventJS;
 import dev.latvian.mods.rhino.util.HideFromJS;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.glfw.GLFW;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT,bus = Mod.EventBusSubscriber.Bus.FORGE,modid = KeyBindJS.MODID)
+import java.util.HashMap;
+
+@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE, modid = KeyBindJS.MODID)
 public class KeyPressedEvent extends ClientEventJS {
 
-    private final String customName;
+    private String customName;
 
     public KeyPressedEvent(String customName) {
         this.customName = customName;
@@ -19,11 +26,31 @@ public class KeyPressedEvent extends ClientEventJS {
 
     @SubscribeEvent
     @HideFromJS
-    public static void keyPressed(TickEvent.ClientTickEvent event) {
-        KeyBindModifyEvent.keyMappingListener.forEach((k, v)->{
-            if(v.consumeClick())
-                KeyBindEvents.KEY_PRESS.post(new KeyPressedEvent(k),k);
+    public static void onKeyPress(InputEvent.Key event) {
+        KeyBindModifyEvent.keyMappingListener.forEach((k, v) -> {
+            if (isDown(event, v)) {
+                if (Minecraft.getInstance().screen == null) {
+                    if (event.getAction()==1)
+                        KeyBindEvents.FIRST_KEY_PRESS.post(new KeyPressedEvent(k), k);
+                    if (event.getAction()==0)
+                        KeyBindEvents.KEY_RELEASE.post(new KeyPressedEvent(k), k);
+                    if (event.getAction()!=0)
+                        KeyBindEvents.KEY_PRESS.post(new KeyPressedEvent(k), k);
+                } else {
+                    if (event.getAction()==1)
+                        KeyBindEvents.FIRST_KEY_PRESS_GUI.post(new KeyPressedEvent(k), k);
+                    if (event.getAction()==GLFW.GLFW_RELEASE)
+                        KeyBindEvents.KEY_RELEASE_GUI.post(new KeyPressedEvent(k), k);
+                    if (event.getAction()!=0)
+                        KeyBindEvents.KEY_PRESS_GUI.post(new KeyPressedEvent(k), k);
+                }
+            }
         });
+    }
+
+    private static Boolean isDown(InputEvent.Key event, KeyMapping keyMapping) {
+        return keyMapping.getKey().getValue() == event.getKey() &&
+                keyMapping.getKeyModifier().compareTo(keyMapping.getKeyModifier()) == event.getModifiers();
     }
 
 
